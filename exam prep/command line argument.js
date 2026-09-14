3e5d8e157546945084dfa7b7c2eecb93ea7883b6
sreenivas pai <sreenivasapai007@gmail.com>
2026-09-15 00:58:30 +0530

    random

---
+++ exam prep/command line argument.js
@@ -0,0 +1,5 @@
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+console.log("First number is :",num1);
+console.log("Second number is :",num2);
---
+++ exam prep/sum.js
@@ -0,0 +1,6 @@
+var a =10;
+var b =50;
+var c =a+b;
+var d =a-b;
+console.log("sUM of a and b is : "+c);
+console.log("DIFFERENCE of a and b is : "+d);
